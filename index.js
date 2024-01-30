890c3139e0d1789d83d3b355f66d7b8c458dc9e1
kyon1214 <nkyon1201@gmail.com>
2024-01-31 01:07:19 +0900

    add function todo-add done

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("OK");
+$("#addTodo").click(function(){
+    const inputTodo = $("#input").val();
+    $("#todoList").append("<li><input type='checkbox'>" + inputTodo + "</li>");
+
+    $("#input").val("");
+});
